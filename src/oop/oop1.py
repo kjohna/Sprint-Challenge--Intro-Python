fada533cc62d7c2d5aefbdb97556ec2577c72573
kjohna <43828763+kjohna@users.noreply.github.com>
2019-04-26 09:14:57 -0600

    oop1 passing tests

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,58 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Vehicle is lowest level class
+
+
+class Vehicle:
+    def __init__(self):
+        pass
+
+# Vehicle is GroundVehicle's base class
+
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+# Ground Vehicle is Car's base class
+
+
+class Car(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+# Ground Vehicle is Motorcycle's base class
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+# Vehicle is FlightVehicle's base class
+
+
+class FlightVehicle(Vehicle):
+    def __init(self):
+        super().__init__()
+        pass
+
+# Flight Vehicle is Airplane's base class
+
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+# FlightVehicle is Starship's base class
+
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
